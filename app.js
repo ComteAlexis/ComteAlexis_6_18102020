6e353a685a74fd968dd8cc8ff1597a73286021fc
Alexis comte <alexis.73400@icloud.com>
2020-10-18 22:47:15 +0200

    changement nom dbName

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path')
 const userRoute = require('./routes/User')
 const sauceRoute = require('./routes/Sauce')
 
-mongoose.connect('mongodb+srv://update:RIcmc6yOp4Ru3iDv@cluster0.we9kr.mongodb.net/<dbname>?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://update:RIcmc6yOp4Ru3iDv@cluster0.we9kr.mongodb.net/Piquante?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
