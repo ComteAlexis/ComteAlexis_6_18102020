98bbfd233a49368c14fd2464e595e31b2b3d1d48
Alexis comte <alexis.73400@icloud.com>
2020-10-20 09:27:55 +0200

    Ajout helmet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path')
+const helmet = require('helmet')
 
 const userRoute = require('./routes/User')
 const sauceRoute = require('./routes/Sauce')
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
     next()
 })
+app.use(helmet())
 
 app.use(bodyParser.json())
 
